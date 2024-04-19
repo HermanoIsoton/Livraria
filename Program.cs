6dbae2a5c4e5683448a2194ce926dd69ac04e74a
aledubiel <hermanoisoton023@gmail.com>
2024-04-19 09:01:44 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,8 +2,29 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("oi mundo");
-    }
+	static void Main(string[] args)
+	{
+		Estoque estoque = new Estoque();
+		estoque.Quantidade = 300;
+		estoque.venderEstoque(20);
+		Console.WriteLine(estoque.Quantidade);
+	}
 }
+
+class Estoque
+{
+	public string Titulo, Autor;
+	public int isbn, Quantidade;
+	
+	public void addEstoque(int quantia)
+	{
+		Quantidade = Quantidade + quantia;
+	}
+	public void venderEstoque(int quantia)  
+	{
+		Quantidade = Quantidade - quantia;
+		
+	}
+	
+}
+
